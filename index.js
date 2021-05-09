4cd8973d03103125d5955ce3c42a2045003a9aa5
Manuel Florez <manuelflorezw@gmail.com>
2021-05-09 16:46:40 -0500

    refactor code

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const { Telegraf } = require('telegraf') 
+const { Telegraf } = require('telegraf')
 
 require('dotenv').config()
 
+const { start } = require('./application/operations/start.js')
+const { help } = require('./application/operations/help.js')
+const { dolar1 } = require('./application/operations/dolar1.js')
+
+const { infoManuel } = require('./application/route/info.js')
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
+
+bot.start( start );
+
+bot.help( help );
+
+bot.hears('admin', infoManuel)
+
+bot.hears('1dolar', dolar1)
+
+bot.hears('date', (ctx) => ctx.reply(new Date().toLocaleString()))
+
+
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
