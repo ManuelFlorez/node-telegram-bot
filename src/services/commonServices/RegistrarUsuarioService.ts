6f4c53a8f543f7fc054df71a13cdc64d92ad9f58
Manuel Florez <manuelflorezw@gmail.com>
2021-06-06 21:59:48 -0500

    refactorizando

--- src/services/commonServices/RegistrarUsuarioService.ts
+++ src/services/commonServices/RegistrarUsuarioService.ts
@@ -2,19 +2,15 @@ import Usuario from '../../models/Usuario';
 import { Message } from './Message';
 import { logger } from '../../util/logger'
 
-const logError = (err: any) => console.log('err', err)
-const logRegisterSuccess = (doc: any) => logger.info('registrar exitoso', doc)
-const logRegisterFaile = (doc: any) => logger.info('Ya hay un registro!', doc)
-
 const tengoRegistro = (message: Message): string => {
-  logRegisterFaile(message)
+  logger.info('Ya hay un registro!')
   return `${message.from.first_name} usted ya tiene un registro!`
 }
 
 const registrar = (message: Message, calback: Function) => {
   const usuario = new Usuario({ name:message.from.first_name, id: message.from.id })
   usuario.save().then(() => {
-    logRegisterSuccess(usuario)
+    logger.info('registrar exitoso')
     calback('Se registro exitosamente')
   },
     (reason) => {
@@ -27,7 +23,8 @@ const registrar = (message: Message, calback: Function) => {
 const registrarUsuarioService = (message: Message, calback: Function) => {
   Usuario.findOne({ 'id':message.from.id }, (err: any, doc: any) => {
     if (err) {
-      logError(err);
+      logger.error('Error al tratar de buscar un usuario')
+      console.log('err', err)
       return
     }
 
